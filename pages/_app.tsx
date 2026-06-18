a8d47ecb0a740ceb271425fc6baaf15a1c1e3a23
GodwinZuberu <onimico4eva@gmail.com>
2026-06-18 20:22:48 +0100

    Fix _app.tsx: add proper App component with getInitialProps to disable ASO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,12 @@
+import App from 'next/app';
+import type { AppProps, AppContext } from 'next/app';
+import '../styles/globals.css';
 
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const ctx = await App.getInitialProps(context);
+  return { ...ctx };
+};
