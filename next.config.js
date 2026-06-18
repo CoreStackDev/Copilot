9efeda8c775b4ec5030abe2528d4120660e98b49
GodwinZuberu <onimico4eva@gmail.com>
2026-06-18 20:23:20 +0100

    Fix next.config.js: add webpack externals for botframework-webchat

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Prevent botframework-webchat from being bundled server-side
+      const externals = Array.isArray(config.externals) ? config.externals : [];
+      config.externals = [...externals, 'botframework-webchat'];
+    }
+    return config;
+  },
+};
 
+module.exports = nextConfig;
